c2c8a337d523cc3c2d409f5ddc701e1589f66ba5
Marcio Woitek <woitek@usp.br>
2021-07-07 11:22:08 -0300

    Exercises - 'Data Types' and 'let, const and var'

---
+++ fundamentals/part_1/data_types.js
@@ -0,0 +1,10 @@
+let country = "Brazil";
+let population = 214734970;
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
---
+++ fundamentals/part_1/let_const_var.js
@@ -0,0 +1,5 @@
+let country = "Brazil";
+let population = 214734970;
+const isIsland = false;
+const language = "Portuguese";
+language = "Japanese";
