5b76c5d2718fb47bfb8045c838b20e7b02e113a8
Marcio Woitek <woitek@usp.br>
2021-07-07 14:58:18 -0300

    Exercise - 'The Conditional (Ternary) Operator'

---
+++ fundamentals/part_1/ternary_operator.js
@@ -0,0 +1,25 @@
+const country = "Brazil";
+let population = 214734970;
+const averagePopulation = 33000000;
+
+console.log(
+    population > averagePopulation
+        ? `${country}'s population is above average`
+        : `${country}'s population is below average`
+);
+
+population = 13000000;
+
+console.log(
+    population > averagePopulation
+        ? `${country}'s population is above average`
+        : `${country}'s population is below average`
+);
+
+population = 130000000;
+
+console.log(
+    population > averagePopulation
+        ? `${country}'s population is above average`
+        : `${country}'s population is below average`
+);
