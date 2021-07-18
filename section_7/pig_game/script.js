0576940fafbe2d6137c57baedfe90655e35e9512
Marcio Woitek <woitek@usp.br>
2021-07-18 19:01:52 -0300

    Pig Game: Switching the Active Player

--- section_7/pig_game/script.js
+++ section_7/pig_game/script.js
@@ -12,12 +12,17 @@ const currentScoreElement1 = document.querySelector('#current--1');
 const scoreElement0 = document.querySelector('#score--0');
 const scoreElement1 = document.querySelector('#score--1');
 
+const playerElement0 = document.querySelector('.player--0');
+const playerElement1 = document.querySelector('.player--1');
+
 // Starting conditions
 diceElement.classList.add('hidden');
 scoreElement0.textContent = 0;
 scoreElement1.textContent = 0;
 
+let activePlayer = 0;
 let currentScore = 0;
+const scores = [0, 0];
 
 // Dice rolling functionality
 btnRoll.addEventListener('click', () => {
@@ -32,8 +37,14 @@ btnRoll.addEventListener('click', () => {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    currentScoreElement0.textContent = currentScore; // TODO Change later
+    document.querySelector(`#current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // Switch to next player
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 1 ? 0 : 1;
+    currentScore = 0;
+    playerElement0.classList.toggle('player--active');
+    playerElement1.classList.toggle('player--active');
   }
 });
